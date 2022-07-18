8095b6d22ad7326968ded4431b492020eecc1a0b
Isaac Philip Cooper <Jordan.P.C.Austin@gmail.com>
2022-07-18 13:16:02 +0100

    ngOnInit / ngOnDestroy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-freeCodeCamp';
+export class AppComponent implements OnInit {
+  intervalSub;
+
+  ngOnInit() {
+    this.intervalSub = setInterval(() => {
+      console.log('Hello from ngOnInit');
+    },1000);
+  }
+
+  ngOnDestroy() {
+    if(this.intervalSub) {
+      clearInterval(this.intervalSub);
+    }
+  }
+  
 }
